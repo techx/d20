1bce0799fc063dcd08f56f7b3acd40a5f18a187b
Kosi Nwabueze <kosinw@mit.edu>
2025-02-26 14:56:45 -0500

    part1

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  innerContainer: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 100,
+    marginBottom: 50,
+    marginLeft: 0,
+    marginRight: 0,
+  }
+})
